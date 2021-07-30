08cfb43eea9a8f284a9a9145082593fe0c91a0c4
izzatwae <bedizzat@gmail.com>
2021-07-30 20:57:53 +0700

    tugas

---
+++ tugas sp 4/soal1.js
@@ -0,0 +1,10 @@
+function introduce(name,age,addres,hobby){
+console.log(" nama saya " + name + " umur saya " + age + " alamat saya di " + addres + " dan saya punya hobi " + hobby)
+}
+
+var name = "Agus";
+var age = 30;
+var address = "Jln. Malioboro, Yogyakarta";
+var hobby = "Gaming";
+ 
+introduce(name, age, address, hobby);
